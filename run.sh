d85ca0cd32abefcbb2f9cc3140dda2086b505599
mohankumargupta <mohangupta@live.com>
2017-05-07 14:01:44 +1000

    better structure, introducing separate files for Ansible variables

--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-#set -x
-#https://github.com/Stouts/Stouts.grafana
 
 ANSIBLE_ROLES_PATH=$(pwd)/roles
 export ANSIBLE_ROLES_PATH
-echo $ANSIBLE_ROLES_PATH
+mkdir -p $ANSIBLE_ROLES_PATH
 ansible-galaxy install geerlingguy.apache
 ansible-galaxy install geerlingguy.mysql
 ansible-galaxy install geerlingguy.php
