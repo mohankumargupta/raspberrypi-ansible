a40ce1b4603cce4e91e86d74db45c3709a2ce738
mohankumargupta <mohangupta@live.com>
2017-09-14 22:36:06 +1000

    jupyter working

--- files/jupyter/jupyter_notebook_config.py
+++ files/jupyter/jupyter_notebook_config.py
@@ -4,6 +4,4 @@ c.NotebookApp.ip = '*'
 c.NotebookApp.base_url = '/jupyter'
 c.NotebookApp.trust_xheaders = True
 c.NotebookApp.tornado_settings = {'static_url_prefix': '/jupyter/static/'}
-
-from notebook.auth import passwd
-c.NotebookApp.password = passwd('')
+c.NotebookApp.token = u''
