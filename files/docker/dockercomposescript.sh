efbd1ecba88807ef12b63b7e866f5f95a0145c57
mohankumargupta <mohangupta@live.com>
2017-09-19 16:19:35 +1000

    need to restart docker service before running docker-compose up

--- files/docker/dockercomposescript.sh
+++ files/docker/dockercomposescript.sh
@@ -2,6 +2,12 @@
 cd /storage/docker
 docker pull calangoclube/rpi-apache
 docker pull portainer/portainer:linux-arm
-docker pull  dastrasmue/rpi-samba:v3
+docker pull dastrasmue/rpi-samba:v3
+docker pull 
+docker pull 
+sleep 5
+systemctl stop docker
+sleep 5
+systemctl start docker
 sleep 5
 docker-compose up -d 2>/dev/null >/dev/null
