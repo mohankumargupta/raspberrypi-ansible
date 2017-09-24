844ee25e0545d78b9d306cf060ee1f23d3b341d1
mohankumargupta <mohangupta@live.com>
2017-09-24 15:16:34 +1000

    attemtping to make it more stable for stretch

--- files/docker/dockercomposescript.sh
+++ files/docker/dockercomposescript.sh
@@ -3,11 +3,10 @@ cd /storage/docker
 docker pull calangoclube/rpi-apache
 docker pull portainer/portainer:linux-arm
 docker pull dastrasmue/rpi-samba:v3
-docker pull 
-docker pull 
 sleep 5
 systemctl stop docker
 sleep 5
+systemctl daemon-reload
 systemctl start docker
-sleep 5
-docker-compose up -d 2>/dev/null >/dev/null
+sleep 10
+docker-compose up -d  >/dev/null
