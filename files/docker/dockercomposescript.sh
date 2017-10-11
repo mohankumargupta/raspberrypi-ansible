53294ba1c997f4891a594358e343edd80a9a3a6e
mohankumargupta <mohangupta@live.com>
2017-10-11 17:43:56 +1100

    it works on stretch now

--- files/docker/dockercomposescript.sh
+++ files/docker/dockercomposescript.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 cd /storage/docker
-docker pull calangoclube/rpi-apache
-docker pull portainer/portainer:linux-arm
-docker pull dastrasmue/rpi-samba:v3
-sleep 5
-systemctl stop docker
-sleep 5
-systemctl daemon-reload
-systemctl start docker
-sleep 10
-docker-compose up -d  >/dev/null
+docker-compose pull
+docker-compose up -d
