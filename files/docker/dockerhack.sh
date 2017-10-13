f7fc49027c76b072b548e999ccf85b56f4f5edbc
mohankumargupta <mohangupta@live.com>
2017-10-13 17:54:26 +1100

    still fighting docker

---
+++ files/docker/dockerhack.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+cd /storage/docker/docker
+dpkg -i docker-engine_17.05.0~ce-0~raspbian-jessie_armhf.deb
+cd /storage/docker
+docker-compose up -d
+sleep 10
+docker-compose down
+sleep 10
+docker-compose up -d
+
+
