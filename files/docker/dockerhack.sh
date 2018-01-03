5b064990dc3c0ef21e3d92a477ca666ec3051763
mohankumargupta <mohangupta@live.com>
2018-01-03 14:44:04 +1100

    still fighting docker

--- files/docker/dockerhack.sh
+++ files/docker/dockerhack.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
-cd /storage/docker/docker
-dpkg -i docker-engine_17.05.0~ce-0~raspbian-jessie_armhf.deb
-cd /storage/docker
-docker-compose up -d
-sleep 10
-docker-compose down
-sleep 10
-docker-compose up -d
+
+apt-get -y install --reinstall docker-ce
+#cd /storage/docker/docker
+#dpkg -i docker-engine_17.05.0~ce-0~raspbian-jessie_armhf.deb
+#cd /storage/docker
+#docker-compose up -d
+#sleep 10
+#docker-compose down
+#sleep 10
+#docker-compose up -d
 
 
